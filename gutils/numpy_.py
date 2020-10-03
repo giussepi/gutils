39b53a57f4fb3eba96046b2e17b0d051505a570e
giussepi <giussepexy@gmail.com>
2020-10-03 17:52:20 +0100

    gutils/numpy_.format_label_matrix

--- gutils/numpy_.py
+++ gutils/numpy_.py
@@ -66,3 +66,35 @@ def normcols(matrix):
     assert isinstance(matrix, np.ndarray)
 
     return matrix/sp.linalg.norm(matrix, axis=0)
+
+
+def format_label_matrix(label_matrix, sorted_labels=None):
+    """
+    Transforms the 1-d label_matrix to 2-D label_matrix and returns it
+
+    Args:
+        label_matrix   (np.ndarray): 1-D array
+        sorted_labels (list, tuple): iterable with labels in the order to be used to create
+                                     the 2-D label matrix
+
+    Returns:
+        label matrix (np.ndarray) with shape (num labels, num samples) e.g.:
+        [0, 1, 0, 2, 2] is returned as:
+
+        [[1 0 1 0 0],
+         [0 1 0 0 0],
+         [0 0 0 1 1]]
+    """
+    assert isinstance(label_matrix, np.ndarray)
+    assert len(label_matrix.shape) == 1
+
+    if sorted_labels is not None:
+        assert isinstance(sorted_labels, (list, tuple))
+
+    labels = sorted_labels if sorted_labels else sorted(set(label_matrix))
+    lmatrix = np.zeros([len(labels), label_matrix.shape[0]], dtype=np.int32)
+
+    for label, row in zip(labels, range(len(labels))):
+        lmatrix[row][label_matrix == label] = 1
+
+    return lmatrix
--- gutils/test/test_numpy_.py
+++ gutils/test/test_numpy_.py
@@ -6,7 +6,7 @@ import unittest
 import numpy as np
 from scipy import linalg
 
-from gutils.numpy_ import colnorms_squared_new, normcols
+from gutils.numpy_ import colnorms_squared_new, normcols, format_label_matrix
 
 
 class MatrixMixin:
@@ -28,6 +28,27 @@ class Test_normcols(MatrixMixin, unittest.TestCase):
     def test_normcols(self):
         self.assertTrue(np.array_equal(
             self.matrix/linalg.norm(self.matrix, axis=0), normcols(self.matrix)))
+
+
+class Test_format_label_matrix(unittest.TestCase):
+
+    def test_format_label_matrix(self):
+        input_label_matrix = np.array([0, 1, 0, 2, 2])
+        output_label_matrix = np.array([[1, 0, 1, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 1, 1]])
+        self.assertTrue(np.array_equal(
+            format_label_matrix(input_label_matrix), output_label_matrix
+        ))
+
+    def test_sorted_labels_args(self):
+        input_label_matrix = np.array([0, 1, 0, 2, 2])
+        sorted_labels = [2, 0, 1]
+        output_label_matrix = np.array([[0, 0, 0, 1, 1], [1, 0, 1, 0, 0], [0, 1, 0, 0, 0]])
+        self.assertFalse(np.array_equal(
+            format_label_matrix(input_label_matrix), output_label_matrix))
+        self.assertTrue(np.array_equal(
+            format_label_matrix(input_label_matrix, sorted_labels),
+            output_label_matrix
+        ))
 
 
 if __name__ == '__main__':
