fa59cb593d765f8bd4c6b20c0b8536a884b996e2
giussepi <giussepexy@gmail.com>
2020-11-22 14:55:54 +0000

    gutils/datasets/utils.py calculate_train_val_percentages & TrainValTestSplit

---
+++ gutils/datasets/test/test_utils.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+""" gutils/datasets/test/test_utils """
+
+import unittest
+
+import numpy as np
+
+from gutils.datasets.utils import calculate_train_val_percentages, TrainValTestSplit
+from gutils.numpy_.numpy_ import LabelMatrixManager
+
+
+class Test_calculate_train_val_percentages(unittest.TestCase):
+
+    def setUp(self):
+        self.train_size = .65
+        self.val_size = .10
+        self.test_size = .25
+        self.num_samples = 1000
+
+    def test_decimal_arguments(self):
+
+        self.assertEqual(
+            (self.train_size*self.num_samples, self.val_size*self.num_samples),
+            tuple(
+                map(
+                    lambda x: x*(self.num_samples*(1-self.test_size)),
+                    calculate_train_val_percentages(self.val_size, self.test_size)
+                )
+            )
+        )
+
+    def test_integer_arguments(self):
+        self.assertEqual(
+            (self.train_size*self.num_samples, self.val_size*self.num_samples),
+            tuple(
+                map(
+                    lambda x: x*(self.num_samples*(1-self.test_size)),
+                    calculate_train_val_percentages(int(self.val_size*100), int(self.test_size*100))
+                )
+            )
+        )
+
+
+class Test_TrainValTestSplit(unittest.TestCase):
+
+    def setUp(self):
+        self.train_size = .65
+        self.val_size = .10
+        self.test_size = .25
+        self.num_samples = 100
+
+    def test_1D_targets(self):
+        samples = np.random.rand(self.num_samples, 3)
+        targets = np.array(range(self.num_samples))
+        # targets = np.array([0, 1]*5)
+
+        x_train, x_val, x_test, y_train, y_val, y_test = TrainValTestSplit(
+            samples, targets, self.val_size, self.test_size)()
+
+        self.assertEqual(x_train.shape[0], 65)
+        self.assertEqual(x_val.shape[0], 10)
+        self.assertEqual(x_test.shape[0], 25)
+
+        self.assertEqual(y_train.shape[0], 65)
+        self.assertEqual(y_val.shape[0], 10)
+        self.assertEqual(y_test.shape[0], 25)
+
+    def test_1D_targets_with_stratify(self):
+        samples = np.random.rand(self.num_samples, 3)
+        targets = np.array([0, 1]*50)
+
+        x_train, x_val, x_test, y_train, y_val, y_test = TrainValTestSplit(
+            samples, targets, self.val_size, self.test_size, stratify=targets)()
+
+        self.assertEqual(x_train.shape[0], 65)
+        self.assertEqual(x_val.shape[0], 10)
+        self.assertEqual(x_test.shape[0], 25)
+
+        self.assertEqual(y_train.shape[0], 65)
+        self.assertEqual(y_val.shape[0], 10)
+        self.assertEqual(y_test.shape[0], 25)
+
+    def test_1D_targets_with_stratify_error(self):
+        samples = np.random.rand(self.num_samples, 3)
+        targets = np.array(range(self.num_samples))
+
+        error_msg = ("The least populated class in y has only 1 member, which is too few. "
+                     "The minimum number of groups for any class cannot be less than 2.")
+
+        with self.assertRaisesRegex(ValueError, error_msg):
+            TrainValTestSplit(
+                samples, targets, self.val_size, self.test_size, stratify=targets)()
+
+    def test_2D_targets(self):
+        samples = np.random.rand(self.num_samples, 3)
+        targets = np.array([0, 1]*50)
+        targets = LabelMatrixManager.get_2d_matrix_from_1d_array(targets, 2).T
+
+        x_train, x_val, x_test, y_train, y_val, y_test = TrainValTestSplit(
+            samples, targets, self.val_size, self.test_size)()
+
+        self.assertEqual(x_train.shape[0], 65)
+        self.assertEqual(x_val.shape[0], 10)
+        self.assertEqual(x_test.shape[0], 25)
+
+        self.assertEqual(y_train.shape[0], 65)
+        self.assertEqual(y_val.shape[0], 10)
+        self.assertEqual(y_test.shape[0], 25)
+
+    def test_2D_targets_with_stratify(self):
+        samples = np.random.rand(self.num_samples, 3)
+        targets = np.array([0, 1]*50)
+        targets = LabelMatrixManager.get_2d_matrix_from_1d_array(targets, 2).T
+
+        x_train, x_val, x_test, y_train, y_val, y_test = TrainValTestSplit(
+            samples, targets, self.val_size, self.test_size, stratify=targets)()
+
+        self.assertEqual(x_train.shape[0], 65)
+        self.assertEqual(x_val.shape[0], 10)
+        self.assertEqual(x_test.shape[0], 25)
+
+        self.assertEqual(y_train.shape[0], 65)
+        self.assertEqual(y_val.shape[0], 10)
+        self.assertEqual(y_test.shape[0], 25)
+
+    def test_2D_targets_with_stratify_error(self):
+        samples = np.random.rand(self.num_samples, 3)
+        targets = np.array(range(self.num_samples))
+        targets = LabelMatrixManager.get_2d_matrix_from_1d_array(targets, self.num_samples).T
+
+        error_msg = ("The least populated class in y has only 1 member, which is too few. "
+                     "The minimum number of groups for any class cannot be less than 2.")
+
+        with self.assertRaisesRegex(ValueError, error_msg):
+            TrainValTestSplit(samples, targets, self.val_size, self.test_size, stratify=targets)()
---
+++ gutils/datasets/utils.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+""" gutils/datasets/utils """
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def calculate_train_val_percentages(val_size, test_size):
+    """
+    Generaly, when using sklearn.model_selection.train_test_split we get the train and test splits.
+    Thus, the validation split use to be calculated by running a second train_test_split over the
+    train split so we end with train_train, train_val and test splits. In this was they val_size is
+    calculated over the train split and not over the whole datastet.
+
+    This function returns the right train_train_size and train_val_size to be used to properly split
+    the train split in a way that val_size is taken from the whole datastet. E.g.:
+
+    total = test_size * total + val_size * total + train_train_size * (total*(1-test_size))
+
+    Thus, if you want to split your dataset in train 65%, val 10% and test 25% the following
+    will be true:
+
+    TOTAL * VAL_SIZE == TOTAL * (1-TEST_SIZE) * TRAIN_VAL_SIZE
+    TOTAL * VAL_SIZE == TOTAL * (1-TEST_SIZE) * VAL_SIZE/(1-TEST_SIZE)
+
+    E.g. If we had a dataset with 100 samples:
+     100  *   0.1    == 100   *   (1-0.25)    *   0.1 / (1-0.25)
+                  10 == 10
+
+    Usage:
+        # first we get the final test split
+        x_train_val, x_test, y_train_val, y_test = train_test_split(myfeatures, mylabels,
+            test_size=.25, ramdom_state=42, stratify=mylabels)
+
+        # second calculate the equivalent percentages in the train_val split
+        train_train_size, train_val_size = calculate_train_val_subdataset_percentage(.1, .25)
+
+        # Finally, we get the final train and validation splits
+        # option 1
+        x_train, x_val, y_train, y_val = train_test_split(x_train_val, y_train_val,
+            train_size=train_train_size, ramdom_state=42, stratify=y_train_val)
+        # option 2
+        x_train, x_val, y_train, y_val = train_test_split(x_train_val, y_train_val,
+            test_size=train_val_size, ramdom_state=42, stratify=y_train_val)
+    """
+    assert isinstance(val_size, (float, int))
+    assert isinstance(test_size, (float, int))
+    assert 0 < val_size < 100
+    assert 0 < test_size < 100
+
+    val_size, test_size = list(map(lambda x: x/100 if x >= 1 else x, (val_size, test_size)))
+
+    train_val_size = val_size/(1-test_size)
+    train_train_size = 1 - train_val_size
+
+    return train_train_size, train_val_size
+
+
+class TrainValTestSplit:
+    """
+    Splits the dataset into train, validation and test subdatasets
+
+    Usage:
+        x_train, x_val, x_test, y_train, y_val, y_test = TrainTestSplit(samples, targets, val_size, test_size)()
+    """
+
+    def __init__(self, samples, targets, val_size=.1, test_size=.2, **kwargs):
+        """
+        Initializes the instance
+
+        Args:
+            samples (np.ndarray): numpy array with shape [<num samples>, <num features>]
+            targets (np.ndarray): numpy array with shape [<num_samples>] or [<num_samples>, <num classes>]
+            val_size     (float): validation dataset size in range [0, 1]
+            test_size    (float): test dataset size in range [0, 1]
+
+        Kwargs:
+            random_stat   e (int): Controls the shuffling applied to the data before applying the split.
+            shuffle        (bool): Whether or not to shuffle the data before splitting.
+            stratify (np.ndarray): If not None, data is split in a stratified fashion, using this as the class labels.
+        """
+        self.samples = samples
+        self.targets = targets
+        self.val_size = val_size
+        self.test_size = test_size
+        self.random_state = kwargs.get('random_state', 42)
+        self.shuffle = kwargs.get('shuffle', True)
+        self.stratify = kwargs.get('stratify', None)
+
+        assert isinstance(self.samples, np.ndarray)
+        assert isinstance(self.targets, np.ndarray)
+        assert isinstance(self.val_size, (float, int))
+        assert isinstance(self.test_size, (float, int))
+        assert isinstance(self.random_state, int)
+        assert isinstance(self.shuffle, bool)
+        assert self.samples.shape[0] == self.targets.shape[0]
+        if self.stratify is not None:
+            assert isinstance(self.stratify, np.ndarray)
+
+    def __call__(self):
+        """ Functor call """
+        return self.__split_dataset()
+
+    def __split_dataset(self):
+        """
+        Splits the dataset into train, validation and test datasets and returns them as
+        NumPy arrays
+
+        Returns:
+            x_train, x_val, x_test, y_train, y_val, y_test
+        """
+
+        x_train_val, x_test, y_train_val, y_test = train_test_split(
+            self.samples, self.targets, test_size=self.test_size,
+            random_state=self.random_state, stratify=self.stratify
+        )
+
+        train_val_size = calculate_train_val_percentages(self.val_size, self.test_size)[1]
+
+        x_train, x_val, y_train, y_val = train_test_split(
+            x_train_val, y_train_val, test_size=train_val_size,
+            random_state=self.random_state,
+            stratify=y_train_val if self.stratify is not None else None
+        )
+
+        return x_train, x_val, x_test, y_train, y_val, y_test
